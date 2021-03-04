fc5db7a48ff943ffb3c6d460aa341ac08890718e
Edward Huang <ejhuang.2015@gmail.com>
2021-03-03 22:54:42 -0500

    Fixed id issue. Added Delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,21 @@
 // =============================================================
 // Express router functions
 const router = require("express").Router();
+// Allows the ability to write and read files etc.
+const fs = require('fs');
+// Allows the generation of unique ids
+const { v4: uuidv4 } = require('uuid');
 // Get access to the Notes class to add, delete, and edit notes
 const Notes = require('../lib/Notes');
+const dbData = require('../db/db');
 
 // API routes
 // =============================================================
 // Get the JSON file
 router.get("/notes", (req, res) => {
     try {
-        res.Notes.readNotes();
+        res.json(dbData);
+        // res.json(Notes.readNotes());
     } catch (err) {
         res.status(500).end();
     }
@@ -18,17 +24,30 @@ router.get("/notes", (req, res) => {
 // Edit the JSON file with the new note
 router.post("/notes", async (req, res) => {
     try {
-        Notes.addNote(req.body.title,req.body.text);
-        res.json(true);
+        const newID = uuidv4();
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: newID,
+        }
+        dbData.push(newNote);
+        console.log(dbData);
+        // Notes.addNote(req.body.title,req.body.text);
+        res.json(dbData);
     } catch (err) {
         res.status(500).end();
     }
 });
 // Delete the note with the specified UUID
 router.delete("/notes/:id", (req, res) => {
+    let noteIndex = dbData.findIndex( element => {
+        return element.id == req.params.id
+    });
+    console.log(noteIndex);
     try {
-        Notes.deleteNote(req.params.id)
-        res.json(true);
+        dbData.splice(noteIndex,1);
+        // Notes.deleteNote(req.params.id)
+        res.json(dbData);
     } catch (err) {
         res.status(500).end();
     }
